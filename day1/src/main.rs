06679bd3c8d70430fd639c669ec4a0f62f7cd3b3
Keith Hunter <keith@abstract.com>
2021-12-08 14:52:44 -0500

    Day 1 part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,8 +4,14 @@ use std::path::Path;
 
 fn number_of_increases(measurements: &[i32]) -> i32 {
     let mut increases = 0i32;
-    for i in 1..measurements.len() {
-        if measurements[i] > measurements[i-1] {
+    for i in 0..measurements.len()-3 {
+        let first_window = &measurements[i..i+3];
+        let second_window = &measurements[i+1..i+4];
+
+        let first_sum = first_window.iter().fold(0, |sum, x| sum + x);
+        let second_sum = second_window.iter().fold(0, |sum, x| sum + x);
+
+        if second_sum > first_sum {
             increases += 1;
         }
     }
@@ -22,13 +28,13 @@ fn read_i32_lines<P>(filename: P) -> Vec<i32> where P: AsRef<Path>, {
 }
 
 #[test]
-fn check_part_1_example() {
+fn check_part_2_example() {
     let input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    assert_eq!(number_of_increases(&input), 7);
+    assert_eq!(number_of_increases(&input), 5);
 }
 
 #[test]
-fn check_part_1() {
+fn check_part_2() {
     let input = read_i32_lines("input.txt");
-    assert_eq!(number_of_increases(&input), 1557);
+    assert_eq!(number_of_increases(&input), 1608);
 }
