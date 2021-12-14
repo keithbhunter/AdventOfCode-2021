919a44d2542f406384ec53c345f0a6cd219f8e75
Keith Hunter <keith@abstract.com>
2021-12-14 12:53:33 -0500

    Day 3 part 2.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,11 @@
 use input;
 use std::collections::HashMap;
 
-fn main() {}
+fn main() {
+    let input = input::read_lines("input.txt");
+    let (oxygen_rating, co2_rating) = calculate_oxygen_co2_ratings(&input);
+    println!("{:?}", oxygen_rating * co2_rating);
+}
 
 const PART_1_EXAMPLE_INPUT: [&str; 12] = [
     "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001",
@@ -53,6 +57,51 @@ fn calculate_gamma_epsilon_rate(input: &[String]) -> (usize, usize) {
     (gamma, epsilon)
 }
 
+fn calculate_oxygen_co2_ratings(input: &[String]) -> (usize, usize) {
+    let mut oxygen = Vec::from(input);
+    let mut carbon = Vec::from(input);
+
+    for i in 0..input.len() {
+        if oxygen.len() > 1 {
+            let (ones_oxygen, zeroes_oxygen) = count_bytes(&oxygen);
+            let oxygen_zero_count = zeroes_oxygen.get(&i).unwrap();
+            let oxygen_one_count = ones_oxygen.get(&i).unwrap();
+
+            let mut oxygen_filter_char = 49;
+            if oxygen_zero_count > oxygen_one_count {
+                oxygen_filter_char = 48;
+            }
+
+            oxygen = oxygen
+                .iter()
+                .filter(|o| o.bytes().nth(i).unwrap() == oxygen_filter_char)
+                .map(|s| String::from(s))
+                .collect();
+        }
+
+        if carbon.len() > 1 {
+            let (ones_carbon, zeroes_carbon) = count_bytes(&carbon);
+            let carbon_zero_count = zeroes_carbon.get(&i).unwrap();
+            let carbon_one_count = ones_carbon.get(&i).unwrap();
+
+            let mut carbon_filter_char = 48;
+            if carbon_one_count < carbon_zero_count {
+                carbon_filter_char = 49;
+            }
+
+            carbon = carbon
+                .iter()
+                .filter(|o| o.bytes().nth(i).unwrap() == carbon_filter_char)
+                .map(|s| String::from(s))
+                .collect();
+        }
+    }
+
+    let oxygen_rating = usize::from_str_radix(oxygen.get(0).unwrap(), 2).unwrap();
+    let carbon_rating = usize::from_str_radix(carbon.get(0).unwrap(), 2).unwrap();
+    (oxygen_rating, carbon_rating)
+}
+
 #[test]
 fn check_part_1_example() {
     let input = PART_1_EXAMPLE_INPUT.map(|s| s.to_string());
@@ -67,4 +116,12 @@ fn check_part_1() {
     let (gamma, epsilon) = calculate_gamma_epsilon_rate(&input);
     assert_eq!((gamma, epsilon), (941, 3154));
     assert_eq!(gamma * epsilon, 2_967_914);
+}
+
+#[test]
+fn check_part_2_example() {
+    let input = PART_1_EXAMPLE_INPUT.map(|s| s.to_string());
+    let (oxygen_rating, co2_rating) = calculate_oxygen_co2_ratings(&input);
+    assert_eq!((oxygen_rating, co2_rating), (23, 10));
+    assert_eq!(oxygen_rating * co2_rating, 230);
 }
